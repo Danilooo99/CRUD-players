3e7c32c16ab8b6678376dea65171ade36796dab8
Danilooo99 <danilo.informatica.99@gmail.com>
2022-03-29 22:49:18 +0100

    First commit - Project

---
+++ src/app/models/player.ts
@@ -0,0 +1,11 @@
+export class Player {
+
+    constructor(
+    public id: number,
+    public name: string,
+    public surname: string,
+    public age: number,
+    public country: string,
+    public position: string,
+    ) {}
+} 
